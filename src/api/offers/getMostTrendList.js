542c00fe55819ddffe6c9a3adb782249528e8e49
CryptoSnowPrince <topdirector2017@gmail.com>
2024-01-25 03:42:07 +0000

    update

--- src/api/offers/getMostTrendList.js
+++ src/api/offers/getMostTrendList.js
@@ -11,10 +11,10 @@ module.exports = async (req_, res_) => {
 
         try {
             const fetchItems = await offer.find({ inscriptionID: { $in: ["972710738cc59f3d25e07a10faef3c89b6e489d11c576d4e51d80c106f6bc1abi0"] } }); // TODO most trending conditions
-            console.log("getMostTrendList fetchItems: ", fetchItems)
+            // console.log("getMostTrendList fetchItems: ", fetchItems)
             if (!fetchItems || fetchItems.length <= 0 || true) {
                 const inscriptionsVal = await inscription.findOne({ btcAccount: "bc1pymwq9x3vulsscpm9306dqn4qcj63qzhasuae6uxxep0s4yw32kzq6jj5jm" })
-                console.log("inscriptionsVal: ", inscriptionsVal)
+                // console.log("inscriptionsVal: ", inscriptionsVal)
                 if (!inscriptionsVal) {
                     return res_.send({ result: false, status: FAIL, message: "getMostTrendList err" });
                 }
